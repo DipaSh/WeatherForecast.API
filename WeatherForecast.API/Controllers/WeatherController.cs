57045b0fd09e1fdd47cf04a8f85aeb823b2c9163
dipas <dipa.b.shah@gmail.com>
2020-05-03 16:08:40 +0530

    Added all the components

---
+++ WeatherForecast.API/Controllers/WeatherController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecast.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+        
+        [Route("temprature/{id}")]
+        [HttpGet]
+        public ActionResult<int> GetTempratureInFahrenheit(int cityId)
+        {
+            var rndNext = new System.Random();
+
+            int temp = rndNext.Next(50, 150);
+
+            return temp;
+        }
+
+        [Route("forecast/{id}")]
+        [HttpGet]
+        public ActionResult<string> GetWeatherForecast(int cityId)
+        {
+            string[] forecasts = { "Sunny", "Rainy", "Cool", "Breezy" };
+            var rndNext = new System.Random();
+
+            int idx = rndNext.Next(4);
+
+            return forecasts[idx];
+        }
+    }
+}
